1bb1442607e5afccf91e858257f5b8d5c0be97e4
xcen3k <xcen3k@gmail.com>
2024-07-24 21:11:44 +0300

    solution for 01 bind

---
+++ homework/05-advanced-functions/01-bind.js
@@ -0,0 +1,13 @@
+const obj1 = {
+    name: 'Alice',
+    greet: function () {
+        console.log(`Hello, my name is ${this.name}`);
+    }
+};
+
+const obj2 = {
+    name: 'Bob'
+};
+
+const greetBob = obj1.greet.bind(obj2);
+greetBob();
